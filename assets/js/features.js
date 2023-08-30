923e89233fc4729875fb3ee57881cd1a0f07d0d5
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2023-08-30 12:53:24 +0300

    added js to features

--- assets/js/features.js
+++ assets/js/features.js
@@ -1,1 +1,26 @@
-const list = document.querySelector('features_list_item')
+// const list = document.getElementsByClassName('features_list_item');
+// list.addEventListener("click", function(){ alert("Hello World!"); });
+
+var text = document.getElementById("content_one"); 
+text.style.display = "block";
+var clicked;
+
+function displayText() {
+    text.style.display = "none";
+    text = document.getElementById("content_one");
+    text.style.display = "block";
+}
+
+function displayText2() {
+    text.style.display = "none";
+    text = document.getElementById("content_two");
+    text.style.display = "block";
+    text.classList.add('clicked');
+}
+
+function displayText3() {
+    text.style.display = "none";
+    text = document.getElementById("content_three");
+    text.style.display = "block";
+}
+
