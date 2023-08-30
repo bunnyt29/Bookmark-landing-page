239efacccadf9e0d6597a3153284b7b032a21d9c
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2023-08-30 19:22:48 +0300

    extensions and FAQ - unfinished

--- assets/js/features.js
+++ assets/js/features.js
@@ -3,15 +3,23 @@
 
 var text = document.getElementById("content_one"); 
 text.style.display = "block";
-var clicked;
+var element = document.getElementById("list_item_one");
+element.classList.add("clicked");
 
 function displayText() {
+    element.classList.remove("clicked");
+    element = document.getElementById("list_item_one");
+    element.classList.add("clicked");
     text.style.display = "none";
     text = document.getElementById("content_one");
     text.style.display = "block";
+    text.classList.add("clicked");
 }
 
 function displayText2() {
+    element.classList.remove("clicked");
+    element = document.getElementById("list_item_two");
+    element.classList.add("clicked");
     text.style.display = "none";
     text = document.getElementById("content_two");
     text.style.display = "block";
@@ -19,6 +27,9 @@ function displayText2() {
 }
 
 function displayText3() {
+    element.classList.remove("clicked");
+    element = document.getElementById("list_item_three");
+    element.classList.add("clicked");
     text.style.display = "none";
     text = document.getElementById("content_three");
     text.style.display = "block";
